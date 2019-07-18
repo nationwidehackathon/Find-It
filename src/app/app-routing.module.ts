8f35c9ebf8934b2ff38ef9b64a2023f46682c823
Jenny Ly <lycb@appstate.edu>
2019-07-18 18:13:30 -0400

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { RouterModule, Route } from '@angular/router';
+import { TileComponent } from './tiles/tiles.component';
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    RouterModule.forRoot(routes)
+  ],
+  exports: [ RouterModule ]
 })
 export class AppRoutingModule { }
