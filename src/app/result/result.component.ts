c9290e1f5601b70359671760a3ef20b80bf934d6
PatricksonWeanquoi <patricksonweanquoi@gmail.com>
2019-07-19 02:23:44 -0400

    Mapped contents to table

--- src/app/result/result.component.ts
+++ src/app/result/result.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit , Input} from '@angular/core';
+import { USERS } from '../mock-user';
 @Component({
   selector: 'app-result',
   templateUrl: './result.component.html',
   styleUrls: ['./result.component.css']
 })
 export class ResultComponent implements OnInit {
+  data=USERS;
   constructor() { }
 
   ngOnInit() {}
